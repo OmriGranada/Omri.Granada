a4dc72f8d2ea536983c3784c3c2c6384b292f890
omriGranada <omri.granada@gmail.com>
2021-11-18 17:04:04 +0200

    Ex5

---
+++ Lesson5_JS.js
@@ -0,0 +1,22 @@
+
+
+    function thankyou()
+    {
+        document.getElementById("button").innerHTML = "Thank You";
+        console.log("thank you");
+
+    }
+
+    var x;
+    var y;
+    x=5;
+    y=7;
+    var text = (x==y);
+    console.log(text);
+
+    function changePic()
+    {
+        document.getElementByID("Tutit").src = "images/run.jpg";
+
+    }
+
