e88ec16c33df1714459aa158910bf3258c9016c2
omriGranada <omri.granada@gmail.com>
2021-11-30 12:00:22 +0200

    exc 6 Omri Granada

--- CVgrid_JS.js
+++ CVgrid_JS.js
@@ -1,2 +1,14 @@
 
             console.log("Omri Granada");
+
+function myFunction(){
+    const inpObj = document.getElementById("age");
+    if(!inpObj.checkValidity())
+    {
+        document.getElementBiId("demo").innerHTML = inpObj.validationMessage;
+    } else
+        {
+            document.getElementById("demo").innerHTML = "Input OK";
+        }
+
+}
---
+++ Lesson6_JS.js
@@ -0,0 +1,44 @@
+/*
+var txt = "sdfksjdfsf";
+console.log(txt);
+
+
+    var person = {
+        firstName = "Alma",
+        lastName = "Or",
+
+        function = fullName() {
+
+        }
+
+    }
+
+
+    const d = new Date();
+    console.log(d);
+    var h = d.getHours();
+
+    if (h<12){
+        greeting = "good morning";
+    }
+    else{
+        greeting = "good evening"
+    }
+
+    console.log(greeting);
+
+
+    function greet(){
+        document.getElementById("p").innerHTML = greeting;
+
+    }
+*/
+
+    function play(){
+        setTimeout(() => {
+            document.getElemetById("img").scr
+        }, 500)
+    }
+
+
+
